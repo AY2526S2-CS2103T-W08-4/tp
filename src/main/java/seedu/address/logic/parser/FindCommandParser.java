71cda5194482f4dbd0a024d82caebf2158085f09
Hack-Zac <isaacjasonabraham@gmail.com>
2026-03-26 14:49:45 +0800

    FindCommand: add support for searching by Membership ID
    FindCommand only supports searching by name via the n/ prefix.
    Gym staff need to look up members directly by their Membership ID.
    
    Let's,
    * add MembershipIdContainsPredicate to match persons by exact
      Membership ID
    * update FindCommandParser to handle the id/ prefix alongside
      the existing n/ prefix
    * update FindCommand to accept Predicate<Person> instead of
      NameContainsKeywordsPredicate, supporting both search modes

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBERSHIP_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.MembershipIdContainsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -20,17 +24,29 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_MEMBERSHIP_ID);
 
-        if (!argMultimap.getValue(PREFIX_NAME).isPresent()
-                || argMultimap.getValue(PREFIX_NAME).get().trim().isEmpty()) {
+        boolean hasName = argMultimap.getValue(PREFIX_NAME).isPresent()
+            && !argMultimap.getValue(PREFIX_NAME).get().trim().isEmpty();
+        boolean hasId = argMultimap.getValue(PREFIX_MEMBERSHIP_ID).isPresent()
+            && !argMultimap.getValue(PREFIX_MEMBERSHIP_ID).get().trim().isEmpty();
+
+        if (hasName == hasId) {
+            // both present or neither present
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = argMultimap.getValue(PREFIX_NAME).get().trim().split("\\s+");
+        Predicate<Person> predicate;
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        if (hasName) {
+            String[] nameKeywords = argMultimap.getValue(PREFIX_NAME).get().trim().split("\\s+");
+            predicate = new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
+        } else {
+            String[] idKeywords = argMultimap.getValue(PREFIX_MEMBERSHIP_ID).get().trim().split("\\s+");
+            predicate = new MembershipIdContainsPredicate(Arrays.asList(idKeywords));
+        }
+
+        return new FindCommand(predicate);
     }
-
 }
