288da6b25717c1e147b8903c4b238b8b6988b55f
Hack-Zac <isaacjasonabraham@gmail.com>
2026-04-13 21:03:38 +0800

    Update Name.java

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -12,7 +12,7 @@ public class Name {
     public static final String MESSAGE_CONSTRAINTS =
         "Names must be 1-50 characters long, start with a letter, "
             + "and may only contain letters, spaces, apostrophes ('), hyphens (-), forward slashes (/) and periods (.). "
-            + "e.g. John Doe, Mary-Jane, O'Brien, Dr. Lim, Nguyễn, 毛泽东";
+            + "e.g. John Doe, Mary-Jane, O'Brien, Dr. Lim, Nguyễn Văn Anh, 小明";
 
     public static final String VALIDATION_REGEX = "^(?=.{1,50}$)[\\p{L}]+(?:[.'/  -]\\s*[\\p{L}]+)*$";
 
